c9389bcfd384b4bef7ff8549a88ba0fe874b4849
Jeremy "Ibi-Wan" Kent "...obi <ibiwan@gmail.com>
2013-08-25 13:25:46 -0700

    user-reviewer confusion

--- ibcomics/reviews/views.py
+++ ibcomics/reviews/views.py
@@ -34,7 +34,7 @@ def detail(request, review_id):
 
 ########################################################################################
 
-def getAndValidateUser(reviewer_id, password):
+def getAndValidateReviewer(reviewer_id, password):
     reviewer = Reviewer.objects.get(pk=reviewer_id)
     user = authenticate(username=reviewer.user.username, password=password)
     return reviewer if user else None
@@ -52,10 +52,10 @@ def writereview(request, comic_id, review_text="Enter Review here...", error_mes
 def savereview(request, comic_id):
     comic = get_object_or_404(Comic, pk=comic_id)
     review_text = request.POST['review_text']
-    user = getAndValidateUser(request.POST['reviewer'], request.POST['password'])
-    if user is None:
+    reviewer = getAndValidateReviewer(request.POST['reviewer'], request.POST['password'])
+    if reviewer is None:
         return writereview(request, comic_id, review_text, "Invalid User or Password")
-    Review(reviewer=user, comic=comic, review_text=review_text, stars=5, pub_date=timezone.now()).save()
+    Review(reviewer=reviewer, comic=comic, review_text=review_text, stars=5, pub_date=timezone.now()).save()
     return HttpResponseRedirect(reverse('comicdetail', args=(comic.id,)))
 
 def deletereview(request, review_id, error_message=None):
@@ -66,12 +66,12 @@ def deletereview(request, review_id, error_message=None):
                                                     'error_message':error_message,})
 
 def confirmdeletereview(request, review_id):
-    review = review = get_object_or_404(Review, pk=review_id)
-    user = getAndValidateUser(request.POST['reviewer'], request.POST['password'])
-    if user is None:
+    review  = get_object_or_404(Review, pk=review_id)
+    reviewer = getAndValidateReviewer(request.POST['reviewer'], request.POST['password'])
+    if reviewer is None:
         return deletereview(request, review_id, "Invalid User or Password")
-    if user != review.reviewer.user:
-        return deletereview(request, review_id, "You can only delete your own reviews" + user + review.reviewer.user)
+    if reviewer != review.reviewer:
+        return deletereview(request, review_id, "You can only delete your own reviews")
     review.delete()
 
 ########################################################################################
@@ -86,8 +86,8 @@ def addcomic(request, comic_name="Comic Name", comic_url="URL to FIRST STRIP of 
 def savecomic(request):
     comic_name = request.POST['comic_name']
     comic_url = request.POST['comic_url']
-    user = getAndValidateUser(request.POST['reviewer'], request.POST['password'])
-    if user is None:
+    reviewer = getAndValidateReviewer(request.POST['reviewer'], request.POST['password'])
+    if reviewer is None:
         return addcomic(request, comic_name, comic_url, "Invalid User or  Password");
     Comic(name=comic_name, url=comic_url).save()
     return HttpResponseRedirect(reverse('comicsindex'))
