0bff8bd6b1cb26f3a2149a9a1a710a97ceeebcfe
Jeremy "Ibi-Wan" Kent "...obi <ibiwan@gmail.com>
2013-08-25 13:38:17 -0700

    bad id

--- ibcomics/reviews/views.py
+++ ibcomics/reviews/views.py
@@ -81,7 +81,7 @@ def confirmdeletereview(request, review_id):
         return deletereview(request, review_id, "Malformed Request; try again")
     else:
         review.delete()
-        return HttpResponseRedirect(reverse('reviewer', args=(reviewer.id,)))
+        return HttpResponseRedirect(reverse('reviewerdetail', args=(reviewer.id,)))
 
 ########################################################################################
 
