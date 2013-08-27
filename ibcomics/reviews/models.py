1b7c90cc1365e40f13e066ac7edf0fcde35a2a2f
Jeremy "Ibi-Wan" Kent "...obi <ibiwan@gmail.com>
2013-08-26 23:25:53 -0700

    next

--- ibcomics/reviews/models.py
+++ ibcomics/reviews/models.py
@@ -9,7 +9,7 @@ class Comic(models.Model):
     def __unicode__(self):
         return self.name
     def average_rating(self):
-        return 2;
+        return self.review_set.all
         accum = 0.0
         for review in self.review_set.all:
             accum += review.stars
