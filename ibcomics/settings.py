f15377f7f9a19d01753d1024710bd4a5c53a7f79
Jeremy "Ibi-Wan" Kent "...obi <ibiwan@gmail.com>
2013-08-30 01:53:56 -0700

    no, it was right

--- ibcomics/settings.py
+++ ibcomics/settings.py
@@ -106,7 +106,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'ibcomics.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'ibcomics.wsgi.application'
