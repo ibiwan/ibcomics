9a564a5cfe8df9fed5907d43fb0644773c1bf4c1
Jeremy "Ibi-Wan" Kent "...obi <ibiwan@gmail.com>
2013-08-31 00:43:10 -0700

    distinguish write/edit saves

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,20 +5,21 @@ from reviews import views
 print "8"
 
 urlpatterns = patterns('',
-    # ex: /reviews/5/
-    url(r'^(?P<review_id>\d+)/$', views.detail, name='detail'),
     # ex: /reviews/writereview/5/
     url(r'^writereview/(?P<comic_id>\d+)/$', views.writereview, name='writereview'),
     # ex: /reviews/editreview/5/
     url(r'^editreview/(?P<review_id>\d+)/$', views.editreview, name='editreview'),
-    # ex: /reviews/savereview/5/
-    url(r'^savereview/(?P<comic_id>\d+)/$', views.savereview, name='savereview'),
-    # ex: /reviews/savereview/5/
+    # ex: /reviews/savereview/5/Write
+    url(r'^savereview/(?P<comic_id>\d+)/(?P<write_edit>[a-zA-Z]+)/$', views.savereview, name='savereview'),
+    # ex: /reviews/deletereview/5/
     url(r'^deletereview/(?P<review_id>\d+)/$', views.deletereview, name='deletereview'),
-    # ex: /reviews/savereview/5/
+    # ex: /reviews/confirmdeletereview/5/
     url(r'^confirmdeletereview/(?P<review_id>\d+)/$', views.confirmdeletereview, name='confirmdeletereview'),
+
     # ex: /reviews/addcomic/
     url(r'^addcomic/$', views.addcomic, name='addcomic'),
+    # ex: /reviews/editcomic/3
+    url(r'^editcomic/(?P<comic_id>\d+)/$', views.addcomic, name='addcomic'),
     # ex: /reviews/savecomic/
     url(r'^savecomic/$', views.savecomic, name='savecomic'),
 
@@ -29,6 +30,8 @@ urlpatterns = patterns('',
     # ex: /reviewers/
     url(r'^reviewers$', views.ReviewerIndexView.as_view(), name='reviewersindex'),
     
+    # ex: /reviews/5/
+    url(r'^(?P<pk>\d+)/$', views.ReviewDetailView.as_view(), name='detail'),
     # ex: /reviews/comic/5/
     url(r'^comic/(?P<pk>\d+)/$', views.ComicDetailView.as_view(), name='comicdetail'),
     # ex: /reviews/reviewer/5/
--- reviews/views.py
+++ reviews/views.py
@@ -8,6 +8,8 @@ from django.core.exceptions import PermissionDenied
 
 from reviews.models import Comic, Reviewer, Review
 
+#################################  LISTS  #############################################
+
 class IndexView(generic.ListView):
     template_name = 'reviews/index.html'
     context_object_name = 'latest_review_list'
@@ -26,6 +28,8 @@ class ReviewerIndexView(generic.ListView):
     def get_queryset(self):
         return Reviewer.objects.order_by('name')
 
+#################################  DETAILS  #############################################
+
 class ComicDetailView(generic.DetailView):
     model = Comic
     template_name = 'reviews/comicdetail.html'
@@ -34,31 +38,32 @@ class ReviewerDetailView(generic.DetailView):
     model = Reviewer
     template_name = 'reviews/reviewerdetail.html'
 
-def detail(request, review_id):
-    review = get_object_or_404(Review, pk=review_id)
-    return render(request, 'reviews/detail.html', {'review':review})
+class ReviewDetailView(generic.DetailView):
+    model = Review
+    template_name = 'reviews/detail.html'
 
-########################################################################################
+#################################  HELPERS  #############################################
 
 def getAndValidateReviewerByUsername(username, password):
     user = authenticate(username=username, password=password)
     reviewer = Reviewer.objects.get(user=user)
     return reviewer if user else None
 
-########################################################################################
+#################################  REVIEW MANIP  #############################################
 
-def writereview(request, comic_id, rating=0, review_text="Enter Review here...", error_message=None):
+def writereview(request, comic_id, rating=0, review_text="Enter Review here...", write_edit='Write', error_message=None):
     comic = get_object_or_404(Comic, pk=comic_id)
-    return render(request, 'reviews/writereview.html', {'comic'        : comic, 
+    return render(request, 'reviews/writereview.html', {'write_edit'   : write_edit,  
+                                                        'comic'        : comic, 
                                                         'review_text'  : review_text,
                                                         'rating'       : rating,
                                                         'error_message': error_message,})
 
 def editreview(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
-    return writereview(request, review.comic.id, review.rating, review.review_text)
+    return writereview(request, review.comic.id, review.stars, review.review_text, write_edit='Edit')
 
-def savereview(request, comic_id):
+def savereview(request, comic_id, write_edit):
     try:
         comic = get_object_or_404(Comic, pk=comic_id)
         review_text = request.POST['review_text']
@@ -70,9 +75,9 @@ def savereview(request, comic_id):
         r.save()
         return HttpResponseRedirect(reverse('comicdetail', args=(comic.id,)))
     except (KeyError):
-        return writereview(request, comic_id, error_message="Malformed Request; try again")
+        return writereview(request, comic_id, write_edit=write_edit, error_message="Malformed Request; try again")
     except (Reviewer.DoesNotExist):
-        return writereview(request, comic_id, rating, review_text, "Invalid User or Password")
+        return writereview(request, comic_id, rating, review_text, write_edit, "Invalid User or Password")
 
 def deletereview(request, review_id, error_message=None):
     review = get_object_or_404(Review, pk=review_id)
@@ -93,12 +98,16 @@ def confirmdeletereview(request, review_id):
     except (Reviewer.DoesNotExist):
         return deletereview(request, review_id, "Invalid User or Password")
 
-########################################################################################
+#################################  COMIC MANIP  #############################################
 
 def addcomic(request, comic_name="Comic Name", comic_url="URL to FIRST STRIP of Comic", error_message=None):
     return render(request, 'reviews/addcomic.html', {'comic_name'    : comic_name,
                                                      'comic_url'     : comic_url,
                                                      'error_message' : error_message,});
+
+def editcomic(request, comic_id):
+    comic = get_object_or_404(Comic, pk=comic_id)
+    return addcomic(request, comic.name, comic.url)
 
 def savecomic(request):
     try:
@@ -106,7 +115,10 @@ def savecomic(request):
         comic_url = request.POST['comic_url']
         reviewer = getAndValidateReviewerByUsername(request.POST['username'],
                                                     request.POST['password'])
-        Comic(name=comic_name, url=comic_url).save()
+        c, created = Comic.objects.get_or_create(name=comic_name)
+        c.url = comic_url
+        c.save()
+        #Comic(name=comic_name, url=comic_url).save()
         return HttpResponseRedirect(reverse('comicsindex'))
     except (KeyError):
         return addcomic(request, error_message="Malformed Request; try again");
