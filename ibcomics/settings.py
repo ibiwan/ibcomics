ef0fe854684bf8e016210c8053ee408e649eb4a6
Jeremy "Ibi-Wan" Kent "...obi <ibiwan@gmail.com>
2013-09-04 23:48:13 -0700

    allow other hosts, fix keyfile

--- ibcomics/settings.py
+++ ibcomics/settings.py
@@ -28,7 +28,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['ibcomics.info']
+ALLOWED_HOSTS = ['ibcomics.info','127.0.0.1','localhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -88,7 +88,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-import keyfile
+from keyfile import *
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
