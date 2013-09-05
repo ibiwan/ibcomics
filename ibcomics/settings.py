227b8ed3573f15e2024d510899b51954f0def749
Jeremy "Ibi-Wan" Kent "...obi <ibiwan@gmail.com>
2013-09-04 23:39:36 -0700

    moved key to untracked file

--- ibcomics/settings.py
+++ ibcomics/settings.py
@@ -88,6 +88,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
+import keyfile
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
